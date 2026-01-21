edefcef1b1230815a89c9cd8e23974d5e93d3332
Richard <codemonkeyricky@gmail.com>
2026-01-21 11:02:10 -0800

    refactor: extract MemoryDatabase to dedicated module
    Move MemoryDatabase class to memory/memory.database.ts for better
    code organization and separation of concerns.

--- src/server/database/interface/database.service.ts
+++ src/server/database/interface/database.service.ts
@@ -1,39 +1,7 @@
 import { User } from '../../auth/auth.types';
 import { DatabaseOperations } from '../interface/database.interface';
 import { PostgresDatabase } from '../postgres/postgres.database';
-
-class MemoryDatabase {
-  private static instance: MemoryDatabase;
-  private db: Map<string, Map<string, any>>;
-
-  private constructor() {
-    this.db = new Map();
-    this.init();
-  }
-
-  public static getInstance(): MemoryDatabase {
-    if (!MemoryDatabase.instance) {
-      MemoryDatabase.instance = new MemoryDatabase();
-    }
-    return MemoryDatabase.instance;
-  }
-
-  private init(): void {
-    if (!this.db.has('users')) this.db.set('users', new Map());
-    if (!this.db.has('session_scores')) this.db.set('session_scores', new Map());
-    if (!this.db.has('user_multipliers')) this.db.set('user_multipliers', new Map());
-  }
-
-  public getTable(tableName: string): Map<string, any> {
-    if (!this.db.has(tableName)) this.db.set(tableName, new Map());
-    return this.db.get(tableName)!;
-  }
-
-  public clear(): void {
-    this.db.clear();
-    this.init();
-  }
-}
+import { MemoryDatabase } from '../memory/memory.database';
 
 export class DatabaseService implements DatabaseOperations {
   private readonly databaseType: 'memory' | 'vercel-postgres';
